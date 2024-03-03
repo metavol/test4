b1d92780166b8a5d5d0ce597baad553604f4fe4d
metavol <metavol.info@gmail.com>
2024-03-03 13:21:27 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,10 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue!" />
-  <HelloWorld msg="How are you?" />
+  <span>
+    <HelloWorld msg="Vite + Vue!" />
+    <HelloWorld msg="How are you?" />
+  </span>
 </template>
 
 <style scoped>
@@ -27,5 +29,8 @@ import HelloWorld from './components/HelloWorld.vue'
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
+}
+.helloworld-container{
+  display: flex;
 }
 </style>
